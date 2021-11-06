2c573d1e14c399b9287d4b5de35df4ff4c930f34
frukus <frukolokoto@gmail.com>
2021-11-05 22:56:56 -0400

    primeros ajustes

--- layouts/default.vue
+++ layouts/default.vue
@@ -105,6 +105,11 @@ export default {
           icon: 'mdi-chart-bubble',
           title: 'Inspire',
           to: '/inspire'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/prueba'
         }
       ],
       miniVariant: false,
